77dbd62eb99abe09a3bf64def4af8e79ca112e7c
Denis <denisyu22824@gmail.com>
2021-11-10 16:33:13 +0200

    project created. Header added

---
+++ components/header/header.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+type Props = { 
+    text: string;
+}
+
+const Logo = () => {
+    return (
+        <>
+            <div className="logo logo__leftSide"/>
+            <div className="logo logo__rightSide"/>
+        </>
+    );
+}
+
+
+const Header = ({ text }: Props) => {
+    return (
+        <div className="headerPanel">
+            <Logo/>
+            <p className="text">{text}</p>
+        </div>
+    );
+}
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+// import type { NextPage } from 'next'
+// import Head from 'next/head'
+// import React from 'react'
+// import Link from 'next/link'
+
+
+import Header from '../components/header/header';
+
+
+const MainPage = () => {
+    return(
+        <Header text="Test task"></Header>
+    )
+}
+
+export default MainPage
